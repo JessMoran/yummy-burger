045443d9f587b11d3be23a638cbb4814847c3783
Jess Moran <jessmoran.dev@gmail.com>
2020-07-29 00:17:11 -0400

    Add heroku settings

--- server.js
+++ server.js
@@ -25,6 +25,18 @@ var connection = mysql.createConnection({
   database: "yummy_burger_db"
 });
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "JessMoran29",
+    database: "burgers_db"
+  });
+};
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
